b1b10aeb4ffd5d9319bbc8029702445329f6c9e8
Derek Crosby <derek.l.crosby@gmail.com>
2025-11-26 16:42:50 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3480612328-634ea",
+  "appId": "1:1028299751706:web:8b09d81315b799389e6ca3",
+  "apiKey": "AIzaSyBOJSIUMHRIOvsRN85dIq-5Ihv69AjtqD4",
+  "authDomain": "studio-3480612328-634ea.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1028299751706"
+};
