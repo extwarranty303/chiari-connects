ef28f6f855b153daceb693e1cae8a6e34fe9cca4
Derek Crosby <derek.l.crosby@gmail.com>
2025-11-28 15:25:50 +0000

    make this the favicon for all pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,6 +31,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="">
       <head>
+        <link rel="icon" href="/ChiariConnectsLogo-blk.png" sizes="any" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
